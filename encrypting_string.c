eab21b0616097ab3c754fd6ec5df1bc0c79e33bf
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-07 17:27:57 +0530

    Create encrypting_string.c

---
+++ encrypting_string.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include<string.h>
+int main() {
+    printf("RA2211042010042\n");
+    //declaring and initializing string 
+   
+    char name[12]="SANYA MATHUR";
+    for(int i=0;i<12;i++)
+    {
+        printf("%c",name[i]-1);//encyrpting string
+    }
+    
+
+}
