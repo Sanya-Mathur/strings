557ba518fa0c67c2602b49ab77110a0b877e7d7b
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-07 17:19:32 +0530

    Create printing_string_with_different_precision.c

---
+++ printing_string_with_different_precision.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main() {
+    printf("RA2211042010042\n");
+    //declaring and initializing string 
+    char name[12]={'S','A','N','Y','A',' ','M','A','T','H','U','R'};
+    printf("%s\n",name);
+    printf("%.5s\n",name);
+        printf("%.8s\n",name);
+          printf("%-9.4s\n",name);
+            printf("%8s\n",name);
+}
