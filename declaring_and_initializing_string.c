82b35b8388c4bfa4af609fce17684cb28885fbec
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-07 17:18:53 +0530

    Update declaring_and_initializing_string.c

--- declaring_and_initializing_string.c
+++ declaring_and_initializing_string.c
@@ -1,7 +1,8 @@
 
 #include <stdio.h>
-printf("RA2211042010042\n");
+
 int main() {
+    printf("RA2211042010042\n");
     //declaring and initializing string 
     char name[12]={'S','A','N','Y','A',' ','M','A','T','H','U','R'};
 }
