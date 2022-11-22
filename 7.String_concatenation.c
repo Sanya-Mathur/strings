c604a418915077f36c4f00f1ee40b6be7de27a07
Sanya-Mathur <113873545+Sanya-Mathur@users.noreply.github.com>
2022-11-22 11:15:48 +0530

    Create 7.String_concatenation.c

---
+++ 7.String_concatenation.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main ()
+{printf("RA2211042010042\n");
+ char str1[12] = "Hello";
+ char str2[12] = "World";
+ strcat( str1, str2);//concats string
+ printf("string concated: %s\n", str1 );
+ 
+ 
+ return 0;
+}
